71d38aefaac75e961b9dc85580f95b24bcb25d7b
syedshafinahmed <shafinahmed.cse@gmail.com>
2025-10-28 16:11:16 +0600

    Use fetch to load data

--- src/Components/Users.jsx
+++ src/Components/Users.jsx
@@ -5,7 +5,19 @@ const Users = ({ usersPromise }) => {
   console.log(users);
   return (
     <div>
-
+      <div>
+        <h3>Add a user</h3>
+        <form>
+          <input type="text" /> <br />
+          <input type="email" /> <br />
+          <button>Add User</button>
+        </form>
+      </div>
+      <div>
+        {
+          users.map(user => <p key={user.id}>Name:{user.name}, Email: {user.email}</p>)
+        }
+      </div>
     </div>
   );
 };
