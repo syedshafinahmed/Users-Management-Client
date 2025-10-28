46a1d5ea3c52c1c2f294260f3efb04d7d2217f89
syedshafinahmed <shafinahmed.cse@gmail.com>
2025-10-28 16:35:18 +0600

    Post method and Send Client data to the server

--- src/Components/Users.jsx
+++ src/Components/Users.jsx
@@ -3,13 +3,35 @@ import React, { use } from 'react';
 const Users = ({ usersPromise }) => {
   const users = use(usersPromise);
   console.log(users);
+  const handleAddUser = (e) => {
+    e.preventDefault();
+    const name = e.target.name.value;
+    const email = e.target.email.value;
+    console.log(name, email);
+
+    const newUser = { name, email };
+
+    //send data to server
+    fetch('http://localhost:3000/users', {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(newUser)
+    })
+      .then(res => res.json())
+      .then(data => {
+        console.log('after post', data);
+      })
+
+  }
   return (
     <div>
       <div>
         <h3>Add a user</h3>
-        <form>
-          <input type="text" /> <br />
-          <input type="email" /> <br />
+        <form onSubmit={handleAddUser}>
+          <input name='name' type="text" /> <br />
+          <input name='email' type="email" /> <br />
           <button>Add User</button>
         </form>
       </div>
